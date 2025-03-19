9e7c98f24beea5bbe5083820f170ae2de8d939b9
rahul1387 <rahultripathi7447@gmail.com>
2025-03-19 15:39:48 +0530

    Strings and their methods.

--- stringsAndMethods.java
+++ stringsAndMethods.java
@@ -28,5 +28,19 @@ public class stringsAndMethods {
         System.out.println("The length of string using first method: " + len);
         //Second method ---> Direct method.
         System.out.println("The length of string using second method: " + name.length());
+
+        // SubStrings of string
+        String place = "England";
+        System.out.println(place.substring(0,3));
+
+        //Searching in string using indexOf() method.
+        System.out.println(""" 
+                The index where 'W' is located is:""" + myStr1.indexOf("W"));
+
+        //Matches() method in java used to check if a string exactly matches a given regular expression.
+        //It gives value in boolean.
+        System.out.println(place.matches("England")); //true
+        System.out.println(place.matches("Eng")); //false
+
     }
 }
