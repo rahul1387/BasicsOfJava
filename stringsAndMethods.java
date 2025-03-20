99560e155abd9bf0d0c7f4548fe20e7b8945ab71
rahul1387 <rahultripathi7447@gmail.com>
2025-03-20 13:12:50 +0530

    Strings and their methods.

--- stringsAndMethods.java
+++ stringsAndMethods.java
@@ -42,5 +42,35 @@ public class stringsAndMethods {
         System.out.println(place.matches("England")); //true
         System.out.println(place.matches("Eng")); //false
 
+        //Trimming in strings trim() ---> removes white spaces
+        String fruit = "   My favourite fruit is Mango   ";
+        System.out.println("Without unwanted spaces in sentence: " + fruit.trim());
+
+        //Replacing characters in Strings
+
+        //1. replace():--> replaces a specific character or word of string.
+        String source = "123abc";
+        System.out.println(source.replace('a', '@'));
+
+        //2. replaceFirst() ---> replaces first character or word,with give character or word
+        String text = "one two three two one";
+        System.out.println("replaceFirst(): " + text.replaceFirst("two", "five"));
+
+        //3. replaceAll() --->  replaceAll() method returns a new String with all matches of the regular expression
+        System.out.println("replaceAll(): " + text.replaceAll("two" , "five"));
+
+        // Splitting the Strings with split() ---> split a String into an array of String objects
+//        String[] occurrence = fruit.split("a");
+//        System.out.println("split(): " + occurrence);
+
+        //Getting characters with charAt() method of string.
+        String theString = "This is a good day to code";
+        System.out.println("The character at index 0 is: " + theString.charAt(0));
+        System.out.println("The character at index 3 is: " + theString.charAt(3));
+
+        //Converting to Uppercase and Lowercase
+        String sentence = "This is UPPERCASE and lowercase";
+        System.out.println("toUpperCase(): " + sentence.toUpperCase());
+        System.out.println("toLowerCase(): " + sentence.toLowerCase());
     }
 }
