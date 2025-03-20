9db59bcb6f397eb9a09574bf4c76b6c2e543bee9
rahul1387 <rahultripathi7447@gmail.com>
2025-03-20 13:52:47 +0530

    Math operations and Math Class in Java.

---
+++ mathOperation.java
@@ -0,0 +1,31 @@
+public class mathOperation {
+    public static void main(String[] args) {
+        //Addition (+)
+        int sum1 = 10 + 20; // adding two constant values
+        int sum2 = sum1 + 30; // adding a variable and a constant
+        int sum3 = sum1 + sum2; // adding two variables
+
+        //Subtraction (-)
+        int diff1 = 200 - 10;
+        int diff2 = diff1 - 5;
+        int diff3 = diff1 - diff2;
+
+        //Multiplication (*)
+        int prod1 = 10 * 20;
+        int prod2 = prod1 * 2;
+        int prod3 = prod1 * prod2;
+
+        //Division (/)
+        int division1 = 100/10;
+        int division2 = division1 / 2;
+        int division3 = division1 / division2;
+
+        //Remainder or Modulo
+        int num1 = 100;
+        int remainder = num1 % 9;
+        System.out.println("Remainder: " + remainder);
+
+        // Java Math Class
+
+    }
+}
