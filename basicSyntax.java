d1f0a66be69ed3ca0a53b6153a68ae58eae5f274
rahul1387 <rahultripathi7447@gmail.com>
2025-03-21 18:40:27 +0530

    basic syntax in java

--- basicSyntax.java
+++ basicSyntax.java
@@ -1,7 +1,13 @@
+import java.util.Scanner;
+
 public class basicSyntax { //---> class in which all the methods can be included.
     public static void main(String[] args) { //---> Main method
         System.out.println("Hello World"); // ---> Use to print output
 //        For taking input from user Scanner or BufferedReader class can be used.
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter your name: ");
+        String name = sc.next(); //---> taken input can be stored in variable.
+        System.out.println("Hello " + name + "!");
 
     }
 }
