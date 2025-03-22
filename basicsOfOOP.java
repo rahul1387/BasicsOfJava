d74ea1090a927df24752006940cfb303cd2aaee9
rahul1387 <rahultripathi7447@gmail.com>
2025-03-22 22:49:36 +0530

    Basics of Object-Oriented programming in java.

---
+++ basicsOfOOP.java
@@ -0,0 +1,12 @@
+public class basicsOfOOP //---> Defining class in java.
+{
+//    Class With Fields
+    //variables that store information about the object are called "fields".
+    public class car
+{
+    public String brand = null; // null means they don't have value.
+    public String model = null;
+    public String color = null;
+}
+
+}
