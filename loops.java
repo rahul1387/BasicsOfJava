f43994ada0a56e5d7155ba3f626203e78608ce2a
rahul1387 <rahultripathi7447@gmail.com>
2025-03-22 18:18:53 +0530

    Loops in java.

---
+++ loops.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class loops {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        //For loop ---> used to run a block of code for a certain number of times.
+        /*for (initialExpression; testExpression; updateExpression) {
+            body of the loop
+        }*/
+
+    }
+}
