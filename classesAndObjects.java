f1bd4eada1e9db01969dd083f670d73a2692d900
rahul1387 <rahultripathi7447@gmail.com>
2025-03-25 11:44:59 +0530

    Classes and objects in  java.

--- classesAndObjects.java
+++ classesAndObjects.java
@@ -1,3 +1,29 @@
+//Creating aur custom class.
+// We cant create one more public class we can only one public class in java.
+class employee {
+    int age ;
+    String name;
+    //creating a method inside my class.
+    public void empDetails(){
+        System.out.println("Employee1 name: " + name);
+        System.out.println("Employee2 age: " + age);
+    }
+}
+class Lamp{
+    //stores the value of light
+    //true if light is on and false if light is off.
+    boolean isOn;
+
+    public void turnOn(){
+        isOn = true;
+        System.out.println("Light on? " + isOn);
+    }
+    public void turnOff(){
+        isOn = false;
+        System.out.println("Light on? " + isOn);
+    }
+}
+
 public class classesAndObjects {
     public static void main(String[] args) {
         //Instantiating the class we created through objects
@@ -15,17 +41,17 @@ public class classesAndObjects {
         myEmp2.age = 27;
         myEmp2.empDetails();
 
+        //creating an object for Lamp
+        Lamp graphite = new Lamp();
+        Lamp halogen = new Lamp();
+
+        // calling the method turnOn for light to turn on.
+        graphite.turnOn();
+
+        //calling the method turnOff for light to turn off.
+        halogen.turnOff();
+
+
     }
 }
 
-//Creating aur custom class.
-// We cant create one more public class we can only one public class in java.
-class employee {
-    int age ;
-    String name;
-    //creating a method inside my class.
-    public void empDetails(){
-        System.out.println("Employee1 name: " + name);
-        System.out.println("Employee2 age: " + age);
-    }
-}
