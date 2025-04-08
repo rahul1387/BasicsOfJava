e03f2135d179b197853ca0ef36105469322d5aec
rahul1387 <rahultripathi7447@gmail.com>
2025-04-08 18:35:29 +0530

    Add untracked files

--- accessModifiers.java
+++ accessModifiers.java
@@ -5,10 +5,18 @@ public class accessModifiers {
         If a method or variable is marked as private , then only code inside the same class can access the variable, or call the method.
          */
 
+<<<<<<< HEAD
         //default (package) Access Modifier ---> default access modifier means  same package of code inside the class
+=======
+        //default (package) Access Modifier ---> default access modifier means  same package of code inside the class.
+>>>>>>> aaa8a74 (Add untracked files)
     }
 
 }
 class clock{
+<<<<<<< HEAD
     private long time = 0;//---> this variable is only accessible in the class clock . That means this variable is not accessible outside the class clock.
+=======
+    private long time = 0;//---> this variable is only accessible in the class clock .That means this variable is not accessible outside the class clock.
+>>>>>>> aaa8a74 (Add untracked files)
 }
